bb1a40e1840588cdceaafc1c6141b4bec7d5fc7c
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-07-24 23:31:34 -0400

    added psuedo code to JS

---
+++ assests/script.js
@@ -0,0 +1,4 @@
+// For my psuedo code I need to make a function to display my questions.
+// I will also need a listner to start a countdown timer when I begin the quiz.
+// then I will need a function to verify answers given are correct and if they are wrong then I need to take time away from the player.
+// at the end of the quiz I then need to display the score and a box for the player to input their name.
