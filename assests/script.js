1081ed2a0739f4c5025679b6f61e2b4dd9845f52
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-07-25 23:52:05 -0400

    added buttons and pseudo code

--- assests/script.js
+++ assests/script.js
@@ -1,7 +1,9 @@
-// For my psuedo code I need to make a function to display my questions.
+// For my psuedo code I need a question and answer bank I need to make a function to start my quiz 
+// after pushing start the button will hide and display my questions.
 // I will also need a listner to start a countdown timer when I begin the quiz.
-// then I will need a function to verify answers given are correct and if they are wrong then I need to take time away from the player.
-// at the end of the quiz I then need to display the score and a box for the player to input their name.
+// Then I will need a function to verify answers given are correct and if they are wrong then I need to take time away from the player.
+// At the end of the quiz I then need to display the score and a box for the player to input their name 
+// This will save the players scores for later viewing.
 
 const questions = [
     {
@@ -12,27 +14,26 @@ const questions = [
             {text: "<javascript>", correct: false},
             {text: "<js>", correct: false},
         ]
-    }
+    },
     {
-        question: "What is the correct JavaScript syntax to change the content of the HTML element below?" 
-        "<p id="demo">This is a demonstration.</p>",
+        question: "Which event occurs when the user clicks on an HTML element?",
 
         answers: [
-            {text: "#demo.innerHTML = ""Hello World!"";", correct: false},
-            {text: "document.getElementById("demo").innerHTML = ""Hello World!"";", correct: true},
-            {text: "document.getElement("p").innerHTML = ""Hello World!"";", correct: false},
-            {text: "document.getElementByName("p").innerHTML = ""Hello World!"";", correct: false},
+            {text: "onchange", correct: false},
+            {text: "onmouseover", correct: false},
+            {text: "onmouseclick", correct: false},
+            {text: "onclick", correct: true},
         ]
-    }
+    },
     {
-        question: "How do you write ""Hello World"" in an alert box?",
+        question: "Which operator is used to assign a value to a variable",
         answers: [
-            {text: "alert(""Hello World"")", correct: true},
-            {text: "alerBox(""Hello World"");", correct: false},
-            {text: "msg(""Hello World"");", correct: false},
-            {text: "msgBox9(""Hello World"");", correct: false},
+            {text: "=", correct: true},
+            {text: "X", correct: false},
+            {text: "*", correct: false},
+            {text: "-", correct: false},
         ]
-    }
+    },
     {
         question: "How to write an IF statement in JavaScript?",
         answers: [
@@ -41,7 +42,7 @@ const questions = [
             {text: "if i=5 then", correct: false},
             {text: "if i=5", correct: false},
         ]
-    }
+    },
     {
         question: "How does a FOR loop start?",
         answers: [
@@ -57,19 +58,3 @@ const questionElement = document.getElementById("question");
 const answerButton = document.getElementById("answer-btn");
 const nextButton = document.getElementById("next-btn");
 
-var currentQuestionIndex = 0;
-var score = 0;
-
-function startQuiz(){
-    currentQuestionIndex = 0;
-    score = 0;
-    nextButton.innerHTML = "Next";
-    showQuestion();
-}
-
-function showQuestion(){
-    var currentQuestion = questions[currentQuestionIndex];
-    var questionNumber = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNumber + ". " + currentQuestion
-    question;
-}
